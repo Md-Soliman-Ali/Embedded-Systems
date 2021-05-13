c952db0a0a94f355efbd53dee1b61488addc2042
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-05-13 12:28:25 +0600

    Add files via upload

---
+++ Light Blink V1/Light_Blink.ino
@@ -0,0 +1,29 @@
+
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+
+void setup()
+{
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+}
+
+
+void loop() 
+{
+  digitalWrite(LED1, HIGH);   
+  delay(1000); 
+  digitalWrite(LED2, HIGH);   
+  delay(1000); 
+  digitalWrite(LED3, HIGH);   
+  delay(1000);                       
+
+  digitalWrite(LED1, LOW);   
+  delay(500); 
+  digitalWrite(LED2, LOW);   
+  delay(500); 
+  digitalWrite(LED3, LOW);   
+  delay(500);                                                          
+}
